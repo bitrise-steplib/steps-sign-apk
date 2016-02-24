0ab5a88c2ad2b2eee7afb0edd4382dc6d08f115c
birmacher <birmacher@gmail.com>
2016-02-24 13:02:45 +0000

    additional options added to jarsigner removed baked algorithm logging improvements

--- step.sh
+++ step.sh
@@ -7,33 +7,29 @@ set -e
 #
 # Input validation
 if [ -z "${apk_path}" ] ; then
-	echo "[!] Missing required input: apk_path"
+	printf "\e[31mError: Missing required input: apk_path\e[0m\n"
 	exit 1
 fi
 
 if [ -z "${keystore_url}" ] ; then
-	echo "[!] Missing required input: keystore_url"
+	printf "\e[31mError: Missing required input: keystore_url\e[0m\n"
 	exit 1
 fi
 
 if [ -z "${keystore_password}" ] ; then
-	echo "[!] Missing required input: keystore_password"
+	printf "\e[31mError: Missing required input: keystore_password\e[0m\n"
 	exit 1
 fi
 
 if [ -z "${keystore_alias}" ] ; then
-	echo "[!] Missing required input: keystore_alias"
+	printf "\e[31mError: Missing required input: keystore_alias\e[0m\n"
 	exit 1
 fi
 
 if [ -z "${private_key_password}" ] ; then
-	echo "[!] Missing required input: private_key_password"
+	printf "\e[31mError: Missing required input: private_key_password\e[0m\n"
 	exit 1
 fi
-
-echo
-echo "(i) Required inputs are provided"
-
 
 #
 # Download keystore
@@ -41,12 +37,9 @@ keystore_path=""
 file_prefix="file://"
 if [[ "${keystore_url}" == ${file_prefix}* ]] ; then
 	keystore_path=${keystore_url#$file_prefix}
-
-	echo
-	echo "==> Using local keystore: ${keystore_path}"
 else
 	echo
-	echo "==> Downloading keystore: ${keystore_url}"
+	printf "\e[34mDownloading keystore: \e[0m${keystore_url}\n"
 
 	tmp_dir=$(mktemp -d)
 	keystore_path="${tmp_dir}/keystore.jks"
@@ -57,61 +50,46 @@ fi
 #
 # Sign apk
 echo
-echo "==> Signing apk"
+printf "\e[34mSigning apk\e[0m\n"
+echo "  Using keystore: ${keystore_path}"
+
 file_name=$(basename "${apk_path}")
 dir_name=$(dirname "${apk_path}")
 tmp_signed_apk_path="${dir_name}/tmp_signed-${file_name}"
 
 jarsigner="/usr/bin/jarsigner"
-
-"${jarsigner}" -verbose \
-  -sigalg SHA1withRSA \
-  -digestalg SHA1 \
+"${jarsigner}" \
   -keystore "${keystore_path}" \
   -storepass "${keystore_password}" \
+	${jarsigner_options} \
   -signedjar "${tmp_signed_apk_path}" "${apk_path}" "${keystore_alias}"
-
 
 #
 # Now zipalign it.
 # The -v parameter tells zipalign to verify the APK afterwards.
-echo
-echo "==> Ziping apk"
+echo "  Aligning the APK"
 zipalign=$(ruby "$THIS_SCRIPT_DIR/export_latest_zipalign.rb")
 if [ $? -ne 0 ] ; then
-	echo
-	echo " (!) Failed to get ziplaign path"
-	echo " (!) error: $zipalign"
+	printf "\e[31mError: ${zipalign}\e[0m\n"
 	exit 1
 fi
-echo "zipalign: $zipalign"
 
 signed_apk_path="${dir_name}/signed-${file_name}"
-
-${zipalign} -f -v 4 ${tmp_signed_apk_path} ${signed_apk_path}
-
+${zipalign} -f 4 ${tmp_signed_apk_path} ${signed_apk_path}
 
 #
 # Verifying
-echo
-echo "==> Verifying the ${signed_apk_path}"
+echo "  Verifying the signed APK"
 out=$(${jarsigner} -verify -verbose -certs ${signed_apk_path})
-if [[ "$out" == *"jar verified."* ]] ; then
-	echo " (i) apk successfully signed"
-else
-	echo "verification out:"
-	echo $out
-	echo " (!) apk isn't signed"
+if [[ "$out" != *"jar verified."* ]] ; then
+	printf "\e[31mError: Failed to sign APK\e[0m\n"
 	exit 1
 fi
 
-
 #
 # Exporting signed ipa
-echo
-echo " (i) The signed apk is now available at: ${signed_apk_path}"
+printf "  \e[32mSigned APK created at path: ${signed_apk_path}\e[0m\n"
 envman add --key BITRISE_SIGNED_APK_PATH --value "${signed_apk_path}"
-
 
 #
 # Cleanup
