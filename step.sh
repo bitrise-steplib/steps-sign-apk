3334d881a4adea0db979bd7f639e8fdca482e556
Krisztián Gödrei <godreikrisztian@gmail.com>
2016-09-28 13:44:19 +0200

    Apk path fix (#19)
    * toolkit, export signed apk into BITRISE_APK_PATH env
    
    * godep save

--- step.sh
+++ step.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
+set -ex
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export THIS_SCRIPT_DIR=$THIS_SCRIPT_DIR
 
-export GO15VENDOREXPERIMENT="1"
-GOPATH="$THIS_SCRIPT_DIR/go" go run "${THIS_SCRIPT_DIR}/go/src/github.com/bitrise-io/sign-apk/main.go"
-exit $?
+go_package_name="github.com/bitrise-steplib/steps-sign-apk"
+
+tmp_gopath_dir="$(mktemp -d)"
+full_package_path="${tmp_gopath_dir}/src/${go_package_name}"
+mkdir -p "${full_package_path}"
+
+rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
+
+export GOPATH="${tmp_gopath_dir}"
+export GO15VENDOREXPERIMENT=1
+go run "${full_package_path}/main.go"
