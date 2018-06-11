bdb1ca787908804f14807bf45e3ddb0be0efbfcc
BirmacherAkos <37296013+BirmacherAkos@users.noreply.github.com>
2018-06-11 12:59:36 +0200

    Signature algorithm name postfix fix. (#29)
    * fix find alg
    
    * clean (+1 squashed commit)
    Squashed commits:
    [e3cbcc4] Clean + log added if the signature algorithm name contains unnecessary postfix (Weak...)
    
    *  - (+1 squashed commit)
    Squashed commits:
    [be0e452] PR clean - fix

--- keystore/keystore.go
+++ keystore/keystore.go
@@ -8,7 +8,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"regexp"
 	"strings"
 
 	"github.com/bitrise-io/go-utils/command"
@@ -202,13 +201,28 @@ func properError(err error, out string) error {
 }
 
 func findSignatureAlgorithm(keystoreData string) (string, error) {
-	exp := regexp.MustCompile(`Signature algorithm name: (.*)`)
-
 	scanner := bufio.NewScanner(strings.NewReader(keystoreData))
+
 	for scanner.Scan() {
-		matches := exp.FindStringSubmatch(scanner.Text())
-		if len(matches) > 1 {
-			return matches[1], nil
+		line := scanner.Text()
+
+		if strings.Contains(line, "Signature algorithm name: ") {
+			split := strings.Split(line, "Signature algorithm name: ")
+
+			if len(split) < 2 {
+				return "", fmt.Errorf("failed to expand signature algorithm from: %s", line)
+			}
+
+			alg := split[1]
+			split = strings.Split(alg, " ")
+
+			if len(split) > 1 {
+				log.Warnf("🚨 Signature algorithm name contains unnecessary postfix: %s", alg)
+				log.Printf("Trimmed signature algorithm name: %s", split[0])
+
+				alg = split[0]
+			}
+			return alg, nil
 		}
 	}
 
