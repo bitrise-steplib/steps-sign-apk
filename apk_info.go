e6d571d326ca40b3bf0ebb5ea1ec33aae54c1fc5
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-09-08 18:16:45 +0200

    Adding automatic page align option

---
+++ apk_info.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+
+	"github.com/avast/apkparser"
+)
+
+type manifest struct {
+	XMLName     xml.Name `xml:"manifest"`
+	Application application
+}
+
+type application struct {
+	XMLName           xml.Name `xml:"application"`
+	ExtractNativeLibs bool     `xml:"extractNativeLibs,attr"` // defaults to false
+}
+
+func parseAPKextractNativeLibs(apkPath string) (bool, error) {
+	var manifestContent bytes.Buffer
+	enc := xml.NewEncoder(&manifestContent)
+	enc.Indent("", "\t")
+
+	zipErr, resErr, manErr := apkparser.ParseApk(apkPath, enc)
+	if zipErr != nil {
+		return false, fmt.Errorf("failed to unzip the APK, error: %s", zipErr)
+	}
+	if resErr != nil {
+		return false, fmt.Errorf("failed to parse resources, error: %s", zipErr)
+	}
+	if manErr != nil {
+		return false, fmt.Errorf("failed to parse AndroidManifest.xml, error: %s", zipErr)
+	}
+
+	var manifest manifest
+	if err := xml.Unmarshal(manifestContent.Bytes(), &manifest); err != nil {
+		return false, fmt.Errorf("failed to unmarshal AndroidManifest.xml, error: %s", err)
+	}
+
+	return manifest.Application.ExtractNativeLibs, nil
+}
---
+++ apk_info_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"reflect"
+	"testing"
+
+	"github.com/bitrise-io/go-utils/command/git"
+	"github.com/bitrise-io/go-utils/log"
+)
+
+func Test_parseAPKextractNativeLibs(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("setup: failed to create temp dir, error: %s", err)
+	}
+
+	defer func() {
+		if err := os.RemoveAll(tmpDir); err != nil {
+			log.Warnf("failed to remove temp dir, error: %s", err)
+		}
+	}()
+
+	gitCommand, err := git.New(tmpDir)
+	if err != nil {
+		t.Fatalf("setup: failed to create git project, error: %s", err)
+	}
+	if err := gitCommand.Clone("https://github.com/bitrise-io/sample-artifacts.git").Run(); err != nil {
+		t.Fatalf("setup: failed to clone test artifact repo, error: %s", err)
+	}
+
+	tests := []struct {
+		name    string
+		apkPath string
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "",
+			apkPath: path.Join(tmpDir, "apks", "app-debug.apk"),
+			want:    false,
+			wantErr: false,
+		},
+		{
+			apkPath: "/Users/lpusok/Develop/samples/android-multiple-test-results-sample-master/app/build/outputs/apk/debug/app-debug.apk",
+			want:    true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseAPKextractNativeLibs(tt.apkPath)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseAPKInfo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseAPKInfo() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
