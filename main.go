296b9076f3b53191fc31e0c0eeb1fcbc3d22f351
Igor Ferreira <igorcferreira@hotmail.com>
2020-09-16 19:05:22 +0200

    Change imoport of apksigner to avoid issues

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/FutureWorkshops/steps-sign-apk/apksigner"
+	"./apksigner"
 
 	"github.com/bitrise-io/go-steputils/stepconf"
 	"github.com/bitrise-io/go-utils/command"
